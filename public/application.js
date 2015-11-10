08972ab06783f96aff238c3cbfe5d9ad6eff0d2b
Alex Tideman <alex.tideman@gmail.com>
2015-11-10 13:43:14 -0700

    Initial commit

---
+++ public/application.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('You have connected!');
+});
+
+
+socket.on('message', function (message) {
+    $('.messages').append(`[${message.username}]: ${message.text} <br>`);
+});
+
+
+$('.send-message').click( function () {
+    socket.send('message', {
+        username: $('.username input[type=text]').val(),
+        text: $('.message input[type=text]').val()
+    });
+});
