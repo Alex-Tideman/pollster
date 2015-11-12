412cff5fe13dc44c02a0a9107322d6604e51bd1b
Alex Tideman <alex.tideman@gmail.com>
2015-11-12 11:34:04 -0700

    Using tutorial stuff

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const app = express();
 const http = require('http').Server(app);
+const express = require('express');
+const _ = require('lodash');
+const app = express();
 const io = require('socket.io')(http);
 const redis = require('redis');
 const client = redis.createClient();
 const bodyParser = require('body-parser');
 const path = require('path');
 const md5 = require('md5');
-const User = require('./views/user');
+
+app.use(express.static('views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('views'));
 app.set('view engine', 'ejs');
 
-
-const polls = {};
+var votes = {};
 
 app.get('/', function (req, res) {
   res.render('pages/index');
 });
 
 app.get('/poll/:id', function(req, res) {
-  var poll = client.hgetall(req.params.id, function(err,dbset) {
-    res.render('pages/poll',
-        {poll: {title: dbset.title,
-        response1: dbset.response1, response2:dbset.response2,
-        response3: dbset.response3, endingTime: dbset.endingTime }
-    });
-  });
+  res.render('pages/poll');
+  //var poll = client.hgetall(req.params.id, function(err,dbset) {
+  //  res.render('pages/poll',
+  //      {poll: {title: dbset.title,
+  //      response1: dbset.response1, response2:dbset.response2,
+  //      response3: dbset.response3, endingTime: dbset.endingTime }
+  //  });
+  //});
 });
 
 app.get('/admin/:id', function(req, res) {
-  var poll = client.hgetall(req.params.id, function(err,dbset) {
-    res.render('pages/admin',
-        {poll: {title: dbset.title,
-          response1: dbset.response1, response2:dbset.response2,
-          response3: dbset.response3, endingTime: dbset.endingTime }
-        });
-  });
+  res.render('pages/admin')
+  //var poll = client.hgetall(req.params.id, function(err,dbset) {
+  //  res.render('pages/admin',
+  //      {poll: {title: dbset.title,
+  //        response1: dbset.response1, response2:dbset.response2,
+  //        response3: dbset.response3, endingTime: dbset.endingTime }
+  //      });
+  //});
 });
 
 app.post('/new-poll', function (req, res) {
@@ -51,23 +53,25 @@ app.post('/new-poll', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  var id = socket.id;
-  var user = new User(id);
+  console.log('A user has connected.', io.engine.clientsCount);
 
-  console.log(user);
+  socket.emit('statusMessage', 'You have connected.');
 
-  socket.on('message', function (channel,vote) {
-    if (channel === 'vote') {
-      //client.hset(id,"vote",vote);
-      //client.hset(id,"user",user.id);
-      console.log({vote: vote, user: user.id });
-      //io.sockets.emit('polls', {vote: 1});
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+  io.sockets.emit('voteCount', countVotes(votes));
+
+  socket.on('message', function (channel,message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      io.sockets.emit('voteCount', countVotes(votes));
+      socket.emit('voteMessage', message);
     }
   });
 
-  socket.on('disconnect', function () {
-    //socket.emit('user disconnected', id)
-    //console.log('Someone has disconnected', id);
+  socket.on('disconnect', function(){
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('userConnection', io.engine.clientsCount);
   });
 });
 
@@ -75,3 +79,20 @@ io.on('connection', function (socket) {
 http.listen(process.env.PORT || 3000, function(){
   console.log('Your server is up and running on Port 3000. Good job!');
 });
+
+function countVotes(votes) {
+  var voteCount = {
+  };
+
+  for (vote in votes) {
+    voteCount[votes[vote]]++
+  }
+
+  var results = "";
+
+  _.forOwn(voteCount, function(count,vote) {
+    results += vote + ":" + count + "  ";
+  });
+
+  return results;
+}
--- views/poll.js
+++ views/poll.js
@@ -1,5 +1,34 @@
 var socket = io();
 
-$('input[name=response]').click( function () {
-    socket.send('vote',$(this).val());
+var connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', function (count) {
+    connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (message) {
+    statusMessage.innerText = message;
+});
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        socket.send('voteCast', this.innerText);
+        socket.send('voteMessage', this.innerText);
+    });
+}
+
+var results = document.querySelector('#vote-results');
+
+socket.on('voteCount', function (voteCount) {
+    results.innerText = voteCount;
+});
+
+var voteMessage = document.querySelector('#vote-message');
+
+socket.on('voteMessage', function (message) {
+    voteMessage.innerText = "You voted: " + message;
 });
