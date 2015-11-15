fa85f4ca374eed6e68b6be38f69e9b87edeadf80
Alex Tideman <alex.tideman@gmail.com>
2015-11-15 12:25:51 -0700

    Add chart.js for results

--- views/poll.js
+++ views/poll.js
@@ -1,5 +1,6 @@
 var socket = io();
 
+
 var connectionCount = document.getElementById('connection-count');
 
 socket.on('usersConnected', function (count) {
@@ -36,4 +37,30 @@ socket.on('voteMessage', function (message) {
 });
 
 
+var ctx = $("#myChart").get(0).getContext("2d");
+// This will get the first returned node in the jQuery collection.
+
+var data = {
+    labels: ["Response 1", "Response 2", "Response 3", "Response 4"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
+var myBarChart = new Chart(ctx).Bar(data, options);
 
